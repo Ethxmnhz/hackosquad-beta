080e3029a6f55b10fb6a2b2241d59ce8829e587b
Shaikh Minhaz <officialmnhz@gmail.com>
2025-05-08 17:22:42 +0530

    rdm update2saaa

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
+      'firebase/firestore': 'firebase/firestore/lite',
+      'firebase/auth': 'firebase/auth/dist/index.esm.js',
+      'firebase/storage': 'firebase/storage/dist/index.esm.js'
+    }
   },
   build: {
-    rollupOptions: {
-      external: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage']
-    }
+    sourcemap: true
   }
 });
